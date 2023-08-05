64c087b10efff1b947bd0c45b3954f54391bdfb9
talha-buyhatke <125451109+talha-buyhatke@users.noreply.github.com>
2023-08-05 21:44:28 +0530

    Delete credentialsCoinchacha.js

--- credentialsCoinchacha.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host: 'mainserver.cwu8m3xtlzvq.ap-southeast-1.rds.amazonaws.com',
-    port: 3306,
-    user: 'onramp-test',
-    password: 'kH2qT6lL1fG9tB4u',
-    // poolSize: 50,
-};
